3c8f8b7a042cb1467d33f70e50744f64f6077ee9
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-11-24 21:00:33 +0100

    Add task and tags in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Tag;
+use App\Task;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,6 +18,36 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
+        $faker = Faker\Factory::create();
+
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
+
         Model::reguard();
+    }
+
+    /**
+     * @param Faker $faker
+     */
+    private function seedTasks($faker)
+    {
+        foreach (range(0,100) as $item) {
+            $task = new Task();
+
+            $task->name = $faker->sentence();
+            $task->done = $faker->boolean();
+            $task->priority = $faker->randomDigit();
+            $task->save();
+        }
+    }
+
+    private function seedTags($faker)
+    {
+        foreach (range(0,100) as $item) {
+            $tag = new Tag();
+
+            $tag->name = $faker->word;
+            $tag->save();
+        }
     }
 }
